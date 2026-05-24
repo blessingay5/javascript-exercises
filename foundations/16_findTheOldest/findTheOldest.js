31c30086246d8bfd5498e57d909fecc4f3b0c403
ayomidebless <blessingay5@gmail.com>
2026-05-24 14:24:23 +0100

    used reduce array method to complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+// find the oldest
+//finds the oldest person if yearOfDeath field is undefined on a non-oldest person
+//finds the oldest person if yearOfDeath field is undefined for the oldest person
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce ((oldest, youngest) => {
+    // firstly Calculate age of the current oldest person
+    const oldestYearOfDeath = oldest.yearOfDeath ?? currentYear;
+    const oldestAge = oldestYearOfDeath - oldest.yearOfBirth;
 
+    //now calculate age of the yongest person
+    const currentYearOfDeath = youngest.yearOfDeath ?? currentYear;
+    const yongestAge = currentYearOfDeath - youngest.yearOfBirth    ;
+
+    return yongestAge > oldestAge ? youngest : oldest;
+  });
 };
+    
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
